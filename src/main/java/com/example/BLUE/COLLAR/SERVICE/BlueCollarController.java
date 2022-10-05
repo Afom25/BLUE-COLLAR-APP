04a51843a659cabeee1a5e1df762ad7209c6540a
Hidri <me@example.com>
2022-10-05 01:41:30 -0500

    this commist will add the service.html

---
+++ src/main/java/com/example/BLUE/COLLAR/SERVICE/BlueCollarController.java
@@ -0,0 +1,30 @@
+package com.example.BLUE.COLLAR.SERVICE;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class BlueCollarController {
+	
+	@GetMapping("/service")
+	public ModelAndView getBluelist() {
+		
+		String viewName = "service";
+		
+		Map<String,Object> model = new HashMap<String,Object>();
+		
+		model.put("numberOfMovies", "123");
+		
+		
+		return new ModelAndView(viewName,model);
+		
+	}
+	
+	
+
+}
