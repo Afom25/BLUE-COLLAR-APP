6271538a921a09e8f2d8f7d45af426256b5600c4
Hidri <me@example.com>
2022-10-04 23:48:52 -0500

    first commit

---
+++ src/main/java/com/example/BLUE/COLLAR/SERVICE/BlueCollarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.BLUE.COLLAR.SERVICE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueCollarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueCollarServiceApplication.class, args);
+	}
+
+}
