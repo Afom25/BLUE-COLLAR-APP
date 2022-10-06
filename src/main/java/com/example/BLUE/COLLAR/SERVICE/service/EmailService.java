8c45994a0d78131ff9adf2f2602632e79d815e7a
Hidri <me@example.com>
2022-10-06 14:57:51 -0500

    this commis this email

---
+++ src/main/java/com/example/BLUE/COLLAR/SERVICE/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.example.BLUE.COLLAR.SERVICE.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    public void sendEmail(String subject,String message,String to){
+
+    }
+}
