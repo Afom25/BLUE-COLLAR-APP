e731b90e33ddca085b844040e94285b16f267c66
Hidri <me@example.com>
2022-10-08 10:40:13 -0500

    this commit will add the locations

--- src/main/java/com/example/BLUE/COLLAR/SERVICE/controller/BlueCollarController.java
+++ src/main/java/com/example/BLUE/COLLAR/SERVICE/controller/BlueCollarController.java
@@ -7,13 +7,7 @@ import java.util.Map;
 
 //import com.example.BLUE.COLLAR.SERVICE.repository.BlueRepository;
 
-import com.example.BLUE.COLLAR.SERVICE.model.ChatMessage;
 import com.example.BLUE.COLLAR.SERVICE.model.Job;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -191,19 +185,6 @@ public class BlueCollarController {
 
 	}
 
-	@MessageMapping("/chat.sendMessage")
-	@SendTo("/topic/public")
-	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-		return chatMessage;
-	}
 
-	@MessageMapping("/chat.addUser")
-	@SendTo("/topic/public")
-	public ChatMessage addUser(@Payload ChatMessage chatMessage,
-							   SimpMessageHeaderAccessor headerAccessor) {
-
-		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-		return chatMessage;
-	}
 
 }
