2db2d7740b331687fe7e35b1003d8042458b3d66
HIDRI A <agthidri24@yahoo.com>
2022-10-17 13:55:53 -0500

    this commit will be js

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,103 @@
+
+
+
+
+$(document).ready(function(){
+    $("#hidri-image").click(function(){
+        $("#detail00").show();
+        $("#hidri-image").hide();
+    })
+    $("#detail00").click(function(){
+        $("#hidri-image").show();
+        $("#detail00").hide();
+    })
+
+    $("#project-img1").click(function(){
+        $("#detail1").show();
+        $("#project-img1").hide();
+    })
+    $("#detail1").click(function(){
+        $("#project-img1").show();
+        $("#detail1").hide();
+    })
+    /** det2 */
+    $("#project-img2").click(function(){
+        $("#detail2").show();
+        $("#project-img2").hide();
+    })
+    $("#detail2").click(function(){
+        $("#project-img2").show();
+        $("#detail2").hide();
+    })
+    $("#project-img3").click(function(){
+        $("#detail3").show();
+        $("#project-img3").hide();
+    })
+    $("#detail3").click(function(){
+        $("#project-img3").show();
+        $("#detail3").hide();
+    })
+    
+    $("#project-img4").click(function(){
+        $("#detail4").show();
+        $("#project-img4").hide();
+    })
+    $("#detail4").click(function(){
+        $("#project-img4").show();
+        $("#detail4").hide();
+    })
+    $("#project-img5").click(function(){
+        $("#detail5").show();
+        $("#project-img5").hide();
+    })
+    $("#detail5").click(function(){
+        $("#project-img5").show();
+        $("#detail5").hide();
+    })
+    $("#project-img6").click(function(){
+        $("#detail6").show();
+        $("#project-img6").hide();
+    })
+    $("#detail6").click(function(){
+        $("#project-img6").show();
+        $("#detail6").hide();
+    })
+    $("#project-img7").click(function(){
+        $("#detail7").show();
+        $("#project-img7").hide();
+    })
+    $("#detail7").click(function(){
+        $("#project-img7").show();
+        $("#detail7").hide();
+    })
+    $("#project-img8").click(function(){
+        $("#detail8").show();
+        $("#project-img8").hide();
+    })
+    $("#detail8").click(function(){
+        $("#project-img8").show();
+        $("#detail8").hide();
+    })
+    $("#project-img9").click(function(){
+        $("#detail9").show();
+        $("#project-img9").hide();
+    })
+    $("#detail9").click(function(){
+        $("#project-img9").show();
+        $("#detail9").hide();
+    })
+    
+
+});
+
+$ (function(){
+    new WOW().init();   
+});
+$(window).on('load',function(){
+    $("#hidri-image").addClass("animated zoomIn");
+    // $(".centered0").addClass("animated pulse");
+    // $(".centered2").addClass("animated zoomIn");
+    // $(".centered3").addClass("animated flash");
+    // $(".centered4").addClass("animated flash");
+
+});
